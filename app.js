f78f67cf3b2c5bfbdbd0b41e013f414c0a3d6733
Maulik Shah <maulik.shah715@gmail.com>
2021-05-12 13:28:26 +0530

    🌵 Added the EJS and CSS files for the home page

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+// MODULE CONSTANTS
 const express = require("express");
+const bodyParser = require("body-parser");
+
+// OTHER GLOBAL CONSTANTS/VARIABLES
+let PORT;
 
 const app = express();
 
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.render("home");
 });
 
-app.listen(process.env.PORT || 3000, () => {
+// STARTING THE SERVER ON THE APPROPRIATE PORT
+if (process.env.PORT) {
+  PORT = process.env.PORT;
+} else {
+  PORT = 3000;
+}
+
+app.listen(PORT, () => {
   console.log("Server running on port 3000 or previously defined port");
 });
