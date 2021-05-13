70dd665c1ca031bce61f1f48c42d0440d2b8b803
Maulik Shah <maulik.shah715@gmail.com>
2021-05-13 13:35:32 +0530

    🗡 Installed mongoose and dotenv and also amde a user.ejs file

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // MODULE CONSTANTS
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 
 // OTHER GLOBAL CONSTANTS/VARIABLES
 let PORT;
+let userLoggedIn = false;
 
 const app = express();
 
@@ -14,6 +16,14 @@ app.set("view engine", "ejs");
 // GET AND POST ROUTES
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
 // STARTING THE SERVER ON THE APPROPRIATE PORT
