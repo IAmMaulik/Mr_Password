05912a2b8252f683fe7bd7c32638adefdb027ab8
Maulik Shah <maulik.shah715@gmail.com>
2021-05-14 12:47:18 +0530

    🚘 New users are pushed into an array now. currently disabled mongodb connection

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 // MODULE CONSTANTS
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const db = require("./db")
+const db = require("./db");
 const app = express();
 
+let users = [];
 // Mongoose code
-const connectedToDb = () => {
-  console.log("Connected to database");
-}
+// const connectedToDb = () => {
+//   console.log("Connected to database");
+// }
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', connectedToDb)
-
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', connectedToDb)
 
 // OTHER GLOBAL CONSTANTS/VARIABLES
 let PORT;
 let userLoggedIn = false;
 
 // SETTING FOR THE MODULES USED
+
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 // GET AND POST ROUTES
@@ -33,6 +35,14 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  users.push({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  console.log(users);
 });
 
 // STARTING THE SERVER ON THE APPROPRIATE PORT
